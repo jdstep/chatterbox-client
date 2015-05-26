c03d42ddfb9c95a75fbf5e1343bfe910d91fc65f
jdstep <jdstep23@gmail.com>
2015-05-25 20:41:27 -0700

    Added room creation, and adds to room list based on messages.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,14 +2,17 @@
 
 var app = {};
 
-var mostRecentMessageTime;
+var mostRecentMessageTime = new Date("0");
 
 app.init = function() {
 
 };
 
+var awesomeBackground = '<script="text/javascript">$("body").css("background-image", "url("http://i.imgur.com/H96T1n8.jpg")")</script>';
+
 app.server = 'https://api.parse.com/1/classes/chatterbox';
 
+// sends the passed in message to the server
 app.send = function(message) {
   $.ajax({
     url: app.server,
@@ -25,12 +28,13 @@ app.send = function(message) {
   });
 };
 
+// gets the most recent 100 messages from the server
 app.fetch = function() {
   $.ajax({
     url: app.server,
     type: 'GET',
     success: function(data) {
-      app.appendNewMessages(data);
+      app.prependNewMessages(data);
       console.log('chatterbox: Message Gotten');
     },
     error: function (data) {
@@ -39,39 +43,47 @@ app.fetch = function() {
   });
 };
 
+// removes all messages from the screen
 app.clearMessages = function() {
   $("#chats").html('');
 };
 
-app.appendNewMessages = function(data) {
-  if (!mostRecentMessageTime) {
-    mostRecentMessageTime = new Date(data.results[data.results.length - 1].createdAt);
-  }
+// prepends new messages retrieved from the server
+// and ignores messages with harmful code
+app.prependNewMessages = function(data) {
 
+  // for each message
   _.each(data.results, function(msg) {
+    // check if the message has malicious codez
     if (app.xssAttackPresent(msg)) {
-      console.log(msg);
+      // console.log(msg);
+    // otherwise as long as the message hasn't been added to the list of messages yet
     } else if (new Date(msg.createdAt) > mostRecentMessageTime) {
+      // prepend the message
       app.addMessage(msg);
+      app.addRoom(msg.roomname);
     }
   });
-
+  // set the most recent message time to the most recent message received.
   mostRecentMessageTime = new Date(data.results[0].createdAt);
 };
 
+// Runs a series of regex tests on each value of the message
+// returns true if the message contains malicious codez
 app.xssAttackPresent = function(msg) {
   if (app.regExTests(msg.username)) {
     return true;
   } else if (app.regExTests(msg.text)) {
     return true;
-  } else if (app.regExTests(msg.roomName)) {
+  } else if (app.regExTests(msg.roomname)) {
     return true;
-  } else {
-    return false;
   }
+  return false;
 };
 
+// Runs a series of regex tests on a given string.
 app.regExTests = function(string) {
+  // can add more regex tests here with ||
   if (secureReg.test(string) ||
       docReg.test(string)) {
     return true;
@@ -79,9 +91,9 @@ app.regExTests = function(string) {
     return false;
   }
 };
-app.addMessage = function(message) {
-  var messageString = "";
 
+// prepends a message to the DOM
+app.addMessage = function(message) {
   $("#chats").prepend('<div class="message">'+
                       '<span class="username">' +
                       message.username +
@@ -92,46 +104,95 @@ app.addMessage = function(message) {
                       '</div>');
 };
 
-app.addRoom = function(roomName) {
-  $("#roomSelect").append('<option class="rooms">' + roomName + '</option>')
+// adds a room to the select field
+app.addRoom = function(roomname) {
+  var existingRooms = $(".rooms").filter(function() {
+    return $(this).text() === roomname;
+  });
+  if(existingRooms.length===0 && roomname !== undefined){
+    $("#roomSelect").append('<option class="rooms">' + roomname + '</option>');
+  }
 };
 
+// adds a friend when you get one
 app.addFriend = function(friend) {
 
 };
 
+// retrieves input values for username, text, and roomname
+// from corresponding DOM elements
+// then sends the message to the AJAX send method
 app.handleSubmit = function() {
-  console.log("I tried to submit something");
+  var username = $('#username').val();
+  var text = $('#message').val();
+  var roomname = 'Your Nightmare';
+  var message = {username: username, text: text, roomname: roomname};
+  app.send(message);
 };
 
-var addUsernameOnClick = function() {
+// listens for a click event on usernames
+// event calls addFriend method
+var readyUsernameOnClick = function() {
   $("#chats").on('click', ".username", function(friend) {
   app.addFriend(friend);
 })};
 
+// listens for a click event on the submit button
+// event triggers submit event
 var readySubmitButtonClick = function() {
   $(".submit").on('click', function(event) {
+    event.preventDefault();
     $(".submit").trigger('submit');
   });
 };
 
+// listens for submit action on the submit button
+// triggers handleSubmit method to prepare and send message
 var readySubmitButtonSubmit = function() {
-  $(".submit").on('submit', function() {
+  $(".submit").on('submit', function(event) {
+    event.preventDefault();
     app.handleSubmit();
   });
 };
 
+// listens for click event on gotify button
+// invokes app.fetch method to retrieve new messages
 var readyGetMessagesClick = function() {
   $(".retrieve").on('click', function() {
-    // debugger;
-    console.log(app.fetch());
+    app.fetch();
   });
 };
 
+var readyRetrieveUsername = function(){
+   var queriedUsername = window.location.search;
+   var name = usernameReg.exec(queriedUsername)[1];
+   $("#username").val(name);
+}
+
+var readyMakeNewRoom = function(){
+  $("#newRoomButton").on('click', function(){
+    app.addRoom($('#newRoomText').val());
+    $('#newRoomText').val("");
+  })
+}
+
+var readyEnterRoom = function(){
+  $('#roomSelect').on("change", function(event){
+
+  });
+}
+// initializing all event listeners
+// and does an initial fetch for messages
 $(document).ready(function() {
-  addUsernameOnClick();
+  readyUsernameOnClick();
   readySubmitButtonClick();
   readySubmitButtonSubmit();
   readyGetMessagesClick();
+  readyRetrieveUsername();
+  readyMakeNewRoom();
+  readyEnterRoom();
   app.fetch();
 });
+
+
+//<script>$("body").css("background-image", "url('http://i.imgur.com/H96T1n8.jpg')")</script>
